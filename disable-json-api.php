a55c4db1a94a7f0ccc470a8d27882380f47769a8
nazar.mandzyk <nazar.mandzyk@perfectial.com>
2018-09-03 10:25:05 +0300

    - orig version

--- disable-json-api.php
+++ disable-json-api.php
@@ -1,18 +1,38 @@
 <?php
 /**
- * Plugin Name: Disable JSON API
+ * Plugin Name: Disable REST API
  * Plugin URI: http://www.binarytemplar.com/disable-json-api
- * Description: Uses the built-in filters of the JSON REST API to disable its functionality
- * Version: 1.1
+ * Description: Disable the use of the JSON REST API on your website to anonymous users
+ * Version: 1.4.3
  * Author: Dave McHale
  * Author URI: http://www.binarytemplar.com
+ * Text Domain: disable-json-api
+ * Domain Path: /languages
  * License: GPL2+
  */
 
-// Filters for WP-API version 1.x
-add_filter('json_enabled', '__return_false');
-add_filter('json_jsonp_enabled', '__return_false');
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
-// Filters for WP-API version 2.x
-add_filter('rest_enabled', '__return_false');
-add_filter('rest_jsonp_enabled', '__return_false');
+// i18n
+add_action( 'init', 'disable_rest_api_load_textdomain' );
+function disable_rest_api_load_textdomain() {
+	load_plugin_textdomain( 'disable-json-api', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+
+// Remove REST API info from head and headers
+remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+
+// WordPress 4.7+ disables the REST API via authentication short-circuit.
+// For versions of WordPress < 4.7, disable the REST API via filters
+if ( version_compare( get_bloginfo( 'version' ), '4.7', '>=' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'classes/disable-rest-api.php' );
+	new Disable_REST_API( __FILE__ );
+} else {
+	require_once( plugin_dir_path( __FILE__ ) . 'functions/legacy.php' );
+	DRA_Disable_Via_Filters();
+}
